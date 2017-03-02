562f2d9a7aa5693a97ebaba0e82a5e3cf0609062
Vinoth <vinoth.archive@outlook.com>
2017-03-02 18:05:11 +0800

    Updatest
    Updatest

--- Chapter2/recommendations.py
+++ Chapter2/recommendations.py
@@ -40,7 +40,7 @@ def sim_distance(prefs,person1,person2):
     
     sum_of_squares=sum([pow(prefs[person1][item]-prefs[person2][item],2)
                         for item in prefs[person1] if item in prefs[person2]])
-    return 1/(1+sum_of_squares)
+    return 1/(1+sqrt(sum_of_squares))
 
 #reload(recommendations)
 
